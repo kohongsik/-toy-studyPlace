10b1d01da9f9a89d11eaf35d9f1ac031b603f88a
sigi <sghs3@naver.com>
2022-03-27 14:48:36 +0900

    confict fix

---
+++ src/main/java/com/hs/studyPlace/StudyPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.hs.studyPlace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyPlaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hs/studyPlace/StudyPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hs.studyPlace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
